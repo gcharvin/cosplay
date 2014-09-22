fa4a82cfdd222aeb4373bc856dab010ccff14b4c
Gilles Charvin <gilles.charvin@gmail.com>
2014-09-22 15:30:54 +0200

    first commit

---
+++ cosplay_rules.m
@@ -0,0 +1,25 @@
+global cosplaySettings
+
+
+% Module data
+cosplaySettings=[];
+cosplaySettings.moduleSeq5={};
+cosplaySettings.moduleSeq3={};
+
+cosplaySettings.moduleSeq5{1}='CGAATGCATCTAGATATCGGATCCCGGTCTCGATCC';
+cosplaySettings.moduleSeq3{1}='GCAGGCCTCTGCAGTCGACGGGCCCGGTCTCTCATA';
+
+cosplaySettings.moduleSeq5{2}='CGAATGCATCTAGATATCGGATCCCGGTCTCGTATG';
+cosplaySettings.moduleSeq3{2}='GCAGGCCTCTGCAGTCGACGGGCCCGGTCTCTGTTC';
+
+cosplaySettings.moduleSeq5{3}='CGAATGCATCTAGATATCGGATCCCGGTCTCGGAAC';
+cosplaySettings.moduleSeq3{3}='GCAGGCCTCTGCAGTCGACGGGCCCGGTCTCTTGCGCCTCC';
+
+cosplaySettings.moduleSeq5{4}='CGAATGCATCTAGATATCGGATCCCGGTCTCGCGCA';
+cosplaySettings.moduleSeq3{4}='GCAGGCCTCTGCAGTCGACGGGCCCGGTCTCTCCTG';
+
+cosplaySettings.moduleSeq5{5}='CGAATGCATCTAGATATCGGATCCCGGTCTCGCAGG';
+cosplaySettings.moduleSeq3{5}='GCAGGCCTCTGCAGTCGACGGGCCCGGTCTCTTGCT';
+
+cosplaySettings.moduleSeq5{6}='CGAATGCATCTAGATATCGGATCCCGGTCTCGAGCA';
+cosplaySettings.moduleSeq3{6}='GCAGGCCTCTGCAGTCGACGGGCCCGGTCTCTATGG';
